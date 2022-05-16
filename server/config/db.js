90b2501595ff3098f06a3fae1cc7b6311636eec4
Thedyson Luzon <luzon.thedyson@gmail.com>
2022-05-16 14:24:51 -0400

    first commit

---
+++ server/config/db.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.HostName = exports.Secret = exports.RemoteURI = exports.LocalURI = void 0;
+exports.LocalURI = "mongodb://localhost:27017/cedardentalcare";
+exports.RemoteURI = "mongodb+srv://thedyson:centennial@cluster0.10i0q.mongodb.net/cedardentalcare?retryWrites=true&w=majority";
+exports.Secret = "NotSoSecret";
+exports.HostName = "localhost";
+//# sourceMappingURL=db.js.map
---
+++ server/config/db.ts
@@ -0,0 +1,4 @@
+export const LocalURI = "mongodb://localhost:27017/cedardentalcare";
+export const RemoteURI = "mongodb+srv://thedyson:centennial@cluster0.10i0q.mongodb.net/cedardentalcare?retryWrites=true&w=majority";
+export const Secret = "NotSoSecret"
+export const HostName = "localhost"
